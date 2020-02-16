1531638f38e202af839d1796213c36dcb9da37a7
rajk9200 <raj.django@gamil.com>
2020-02-16 13:13:09 +0530

    all file

---
+++ dd.py
@@ -0,0 +1,9 @@
+
+
+ui =5
+
+
+
+mm =meraFact(ui)
+print(mm)
+
---
+++ mypost/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def add_post(request):
+    return render(request,'posts/addpost.html')
